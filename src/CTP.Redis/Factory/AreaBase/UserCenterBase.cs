fecba6e641c97cc71a9760702fd452e0a6ca1b6b
13250 <13250@DESKTOP-C7DEC61>
2017-06-13 10:01:24 +0800

    框架

--- src/CTP.Redis/Factory/AreaBase/UserCenterBase.cs
+++ src/CTP.Redis/Factory/AreaBase/UserCenterBase.cs
@@ -16,13 +16,34 @@ namespace CTP.Redis.Factory.UserCenter
             string key = string.Empty;
             Type t = this.GetType();
             switch (t.Name)
-            {    
+            {
                 ///
                 case "RegisterUserFactory":
                     key = "registerUser";
                     break;
                 case "AuthLoginUserFactory":
                     key = "authLoginUser";
+                    break;
+                case "AppAccInterfaceFactory":
+                    key = "appAccInterface";
+                    break;
+                case "LoginLogFactory":
+                    key = "loginLog";
+                    break;
+                case "RegisterAppFactory":
+                    key = "registerApp";
+                    break;
+                case "UoperLogFactory":
+                    key = "uoperLog";
+                    break;
+                case "UserConfigFactory":
+                    key = "userConfig";
+                    break;
+                case "UserExtinfoFactory":
+                    key = "userExtinfo";
+                    break;
+                case "UserInterfaceFactory":
+                    key = "userInterface";
                     break;
                 default:
                     break;
